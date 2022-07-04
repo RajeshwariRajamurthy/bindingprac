56ce0e3a6dd569bfde4ef69ef54fb0d559ea0a5a
RajeshwariRajamurthy <108650414+RajeshwariRajamurthy@users.noreply.github.com>
2022-07-04 11:29:24 +0530

    Update MainActivity.java

--- app/src/main/java/com/ref/bindingprac/MainActivity.java
+++ app/src/main/java/com/ref/bindingprac/MainActivity.java
@@ -15,7 +15,7 @@ ActivityMainBinding activityMainBinding;
 
         activityMainBinding= DataBindingUtil.setContentView(this,R.layout.activity_main);
         activityMainBinding.tv.setText("hello");
-        //this is update
+        //this is update I am from website
 
     }
 }
